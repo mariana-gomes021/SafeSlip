bc95e7e3c972ac1c44f7da7dcd80b2c4c99d8642
Maísa de Godoy Ebina <maisaebina@gmail.com>
2025-05-29 15:13:47 -0300

    teste maisa

--- src/safeslip/SafeSlip.java
+++ src/safeslip/SafeSlip.java
@@ -18,7 +18,7 @@ public class SafeSlip {
     public static void main(String[] args) {
         // TODO code application logic here
         //Teste envio do boleto
-        //teste mariana - develop
+        //teste maisa - develop
           System.out.println("===== SafeSlip - Envio de Boleto =====");
 
         EnvioBoleto envio = new EnvioBoleto();
